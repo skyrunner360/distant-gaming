2f0a438ecc95e9111139de86008bd7f446fdcb95
Rishi Mathur <mathurrishi31@gmail.com>
2022-08-31 15:03:21 +0530

    added basic navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import DrawerAppBar from '../Components/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    {/* aka Navbar */}
+    <DrawerAppBar/> 
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
